03e59e2c215d01329c80ae932890ce90941d303a
biljana lukovic <biljana.lukovic@securekey.com>
2017-07-05 10:44:14 -0400

    [SNAP-7]Transaction snap
    Change-Id: I95d0dbdac77853cd68879cab91f00e66d5e5cbf5
    Signed-off-by: biljana lukovic <biljana.lukovic@securekey.com>

---
+++ pkg/snaps/transactionsnap/transactionsnap_test.go
@@ -0,0 +1,72 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package transactionsnap
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	protos "github.com/securekey/fabric-snaps/pkg/snaps/transactionsnap/protos"
+)
+
+func TestTransactionSnapInit(t *testing.T) {
+	snap := &TxSnapImpl{}
+	stub := shim.NewMockStub("transactionsnap", snap)
+
+	snapName := "transactionsnap"
+	snapURL := ""
+
+	var args [][]byte
+	args = append(args, []byte(snapName))
+	args = append(args, []byte(snapURL))
+
+	response := stub.MockInit("TxID", args)
+
+	if response.Status != shim.OK {
+		t.Fatalf("Expecting response status %d but got %d", shim.OK, response.Status)
+	}
+	if response.Message != "" {
+		t.Fatalf("Expecting no response message but got %s", response.Message)
+	}
+
+}
+func TestTransactionSnapInvoke(t *testing.T) {
+	snap := NewSnap()
+
+	transientMap := make(map[string][]byte)
+	transientMap["key"] = []byte("transientvalue")
+	endorserArgs := make([][]byte, 5)
+	endorserArgs[0] = []byte("invoke")
+	endorserArgs[1] = []byte("move")
+	endorserArgs[2] = []byte("a")
+	endorserArgs[3] = []byte("b")
+	endorserArgs[4] = []byte("1")
+	additionalCCIDs := []string{"additionalccid"}
+
+	snapTxReq := protos.SnapTransactionRequest{ChannelID: "testChannel", ChaincodeID: "ccid", TransientMap: transientMap, EndorserArgs: endorserArgs, AdditionalCCIDs: additionalCCIDs}
+	snapTxReqB, err := json.Marshal(snapTxReq)
+
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+
+	var args [][]byte
+	args = append(args, []byte("endorseTransaction"))
+	args = append(args, snapTxReqB)
+	stub := shim.NewMockStub("transactionsnap", snap)
+	//initialize fabric-client
+	response := stub.MockInit("TxID", args)
+	//invoke transaction snap
+	response = stub.MockInvoke("TxID", args)
+
+	if response.Status != shim.ERROR {
+		t.Fatalf("TestTransactionSnapInvoke failed. Expected response status %d but got %d", shim.ERROR, response.Status)
+	}
+}
---
+++ vendor/github.com/hyperledger/fabric-sdk-go/api/peer.go
@@ -0,0 +1,39 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package api
+
+import (
+	"encoding/pem"
+)
+
+// The Peer class represents a peer in the target blockchain network to which
+// HFC sends endorsement proposals, transaction ordering or query requests.
+//
+// The Peer class represents the remote Peer node and its network membership materials,
+// aka the ECert used to verify signatures. Peer membership represents organizations,
+// unlike User membership which represents individuals.
+//
+// When constructed, a Peer instance can be designated as an event source, in which case
+// a “eventSourceUrl” attribute should be configured. This allows the SDK to automatically
+// attach transaction event listeners to the event stream.
+//
+// It should be noted that Peer event streams function at the Peer level and not at the
+// channel and chaincode levels.
+type Peer interface {
+	ConnectEventSource()
+	IsEventListened(event string, channel Channel) (bool, error)
+	AddListener(eventType string, eventTypeData interface{}, eventCallback interface{}) (string, error)
+	RemoveListener(eventListenerRef string) (bool, error)
+	Name() string
+	SetName(name string)
+	Roles() []string
+	SetRoles(roles []string)
+	EnrollmentCertificate() *pem.Block
+	SetEnrollmentCertificate(pem *pem.Block)
+	URL() string
+	SendProposal(proposal *TransactionProposal) (*TransactionProposalResponse, error)
+}
