3f33cd77a9cc1b9b14c13ad3928827eb19198c25
Firas Qutishat <firas.qutishat@securekey.com>
2017-08-15 12:51:22 -0400

    WIP
    Change-Id: If83bd55c06cd0898bb97a0ef9095ab973f568872
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- bddtests/fixtures/config/snaps/httpsnap/cds.sh
+++ bddtests/fixtures/config/snaps/httpsnap/cds.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 NAME=httpsnap
-CDS=$GOPATH/src/github.com/securekey/fabric-snaps/bddtests/fixtures/config/extsysccs/$NAME.golang
+CDS=/opt/extsysccs/$NAME.golang
 SOURCE=github.com/securekey/fabric-snaps/httpsnap/cmd
 
-$GOPATH/src/github.com/hyperledger/fabric/build/bin/peer chaincode package -n $NAME -p $SOURCE -v 1.0.0 $CDS
+peer chaincode package -n $NAME -p $SOURCE -v 1.0.0 $CDS
 
 chmod 775 $CDS
