21a4b0f3e07d13428be8994da12d5dbe9dd3a264
Divyank Katira <Divyank.Katira@securekey.com>
2017-08-31 09:42:42 -0400

    [DEV-2524] Add logging fix, read peer core.yaml
    Change-Id: Iebdd6e80af233774958eec98559d1a5144fc683e
    Signed-off-by: Divyank Katira <Divyank.Katira@securekey.com>

--- scripts/check_license.sh
+++ scripts/check_license.sh
@@ -18,15 +18,13 @@ if [[ -z "$CHECK" ]]; then
     | grep -v .md$  | grep -v .crt$ | grep -v vendor/ | grep -v ^build/ | grep -v .pb.go$ | sort -u)
 fi
 
-echo "Checking committed files for SPDX-License-Identifier headers ..."
-missing=`echo $CHECK | xargs grep -L "SPDX-License-Identifier"`
+echo "Checking committed files for Copyright headers ..."
+missing=`echo $CHECK | xargs grep -L "Copyright"`
 if [ -z "$missing" ]; then
-   echo "All files have SPDX-License-Identifier headers"
+   echo "All files have Copyright headers"
    exit 0
 fi
-echo "The following files are missing SPDX-License-Identifier headers:"
+echo "The following files are missing Copyright headers:"
 echo "$missing"
 echo
-echo "Please replace the Apache license header comment text with:"
-echo "SPDX-License-Identifier: Apache-2.0"
 exit 1
