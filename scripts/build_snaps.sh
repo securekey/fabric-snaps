01c05be66c3422defe86473b3bb64a11fb0e8449
Firas Qutishat <firas.qutishat@securekey.com>
2017-09-05 15:40:56 -0400

    Add fabric-tools image to build snaps
    Change-Id: Iecc3e41ab6a300d112fdc8585d2dc0666bbfa41a
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/build_snaps.sh
+++ scripts/build_snaps.sh
@@ -5,9 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-wget -qO- https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz | tar -xzC /usr/local/
-export PATH=/usr/local/go/bin:$PATH
-export GOPATH=/opt/gopath
 peer chaincode package -n httpsnap -p github.com/securekey/fabric-snaps/httpsnap/cmd -v 1.1.0 /opt/snaps/httpsnap.golang
 peer chaincode package -n txnsnap -p github.com/securekey/fabric-snaps/transactionsnap/cmd -v 1.1.0 /opt/snaps/txnsnap.golang
 /bin/chmod 775 /opt/snaps/*
