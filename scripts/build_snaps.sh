c3cf198d454b797fc654c74c1817ec5493883f4d
Firas Qutishat <firas.qutishat@securekey.com>
2017-09-12 13:04:01 -0400

    [SNAPS-19] Update vendoring to dep
    Change-Id: I6db9a3a9ad73b2d4874099eb2a6ad22534046068
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/build_snaps.sh
+++ scripts/build_snaps.sh
@@ -7,6 +7,6 @@
 
 go build -ldflags "-linkmode external -extldflags '-static'" -o /opt/snapsbinary/httpsnap /opt/gopath/src/github.com/securekey/fabric-snaps/httpsnap/cmd/httpsnap.go
 go build -ldflags "-linkmode external -extldflags '-static'" -o /opt/snapsbinary/transactionsnap  /opt/gopath/src/github.com/securekey/fabric-snaps/transactionsnap/cmd/transactionsnap.go
-peer chaincode package -n httpsnap -p /opt/snapsbinary/httpsnap -v 1.1.0-snapshot-51724a4 /opt/snaps/httpsnap.golang -l binary
-peer chaincode package -n txnsnap -p /opt/snapsbinary/transactionsnap -v 1.1.0-snapshot-51724a4 /opt/snaps/txnsnap.golang -l binary
+peer chaincode package -n httpsnap -p /opt/snapsbinary/httpsnap -v $FABRIC_VERSION /opt/snaps/httpsnap.golang -l binary
+peer chaincode package -n txnsnap -p /opt/snapsbinary/transactionsnap -v $FABRIC_VERSION /opt/snaps/txnsnap.golang -l binary
 /bin/chmod 775 /opt/snaps/*
