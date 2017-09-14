abf73ca0529adb9020ac3cc3184db4fcae8eed8e
Divyank Katira <Divyank.Katira@securekey.com>
2017-09-14 15:21:19 -0400

    [SNAPS-20] Add test HTTP server
    Change-Id: I9c1ad3a7c003a6ad6078ade980eeb32706c6fc1e
    Signed-off-by: Divyank Katira <Divyank.Katira@securekey.com>

---
+++ bddtests/fixtures/httpserver/dummyserver.go
@@ -0,0 +1,84 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	go startTestHTTPServer()
+	for {
+	}
+}
+
+func startTestHTTPServer() {
+
+	initHTTPServerConfig()
+
+	// Register request handlers
+	http.HandleFunc("/hello", HelloServer)
+
+	caCert, err := ioutil.ReadFile(viper.GetString("http.tls.caCert.file"))
+	if err != nil {
+		fmt.Println("HTTP Server: Failed to read ca-cert. " + err.Error())
+		os.Exit(1)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Setup HTTPS client
+	tlsConfig := &tls.Config{
+		ClientCAs:  caCertPool,
+		ClientAuth: tls.RequireAndVerifyClientCert,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	server := &http.Server{
+		Addr:      viper.GetString("http.listen.address"),
+		TLSConfig: tlsConfig,
+	}
+
+	err = server.ListenAndServeTLS(viper.GetString("http.tls.cert.file"), viper.GetString("http.tls.key.file"))
+
+	if err != nil {
+		fmt.Println("HTTP Server: Failed to start. " + err.Error())
+		os.Exit(1)
+	}
+
+}
+
+// HelloServer greeting (JSON)
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"description": "Hello"}`)
+}
+
+func initHTTPServerConfig() {
+	replacer := strings.NewReplacer(".", "_")
+
+	viper.AddConfigPath("./")
+	viper.SetConfigName("config")
+	viper.SetEnvPrefix("core")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(replacer)
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("Fatal error reading config file: %s \n", err)
+		os.Exit(1)
+	}
+}
