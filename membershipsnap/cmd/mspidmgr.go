996e84f6bcdfd793ad24d217f38609a824802471
Bob Stasyszyn <bob.stasyszyn@securekey.com>
2017-11-03 09:58:49 -0400

    [DEV-3786] - Add Membership snap
    - Implement membership snap such that changes
    to Gossip interface are not required
    
    Change-Id: Id86e558ebfee2eb4b9f395c7880f2b72f74ecb67
    Signed-off-by: Bob Stasyszyn <bob.stasyszyn@securekey.com>

---
+++ membershipsnap/cmd/mspidmgr.go
@@ -0,0 +1,96 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"sync"
+
+	"github.com/golang/protobuf/proto"
+	gcommon "github.com/hyperledger/fabric/gossip/common"
+	"github.com/hyperledger/fabric/gossip/service"
+	gossip "github.com/hyperledger/fabric/protos/gossip"
+	"github.com/hyperledger/fabric/protos/msp"
+)
+
+// mspIDMgr manages a map of PKI IDs to MSP IDs. The map is
+// populated dynamically from Gossip.
+type mspIDMgr struct {
+	mspIDMap map[string]string
+	mutex    sync.RWMutex
+}
+
+func newMSPIDMgr(service service.GossipService) *mspIDMgr {
+	m := &mspIDMgr{}
+
+	m.mspIDMap = make(map[string]string)
+
+	_, msgch := service.Accept(func(o interface{}) bool {
+		m := o.(gossip.ReceivedMessage).GetGossipMessage()
+		if m.IsPullMsg() && m.GetPullMsgType() == gossip.PullMsgType_IDENTITY_MSG {
+			return m.IsDataUpdate()
+		}
+		return false
+	}, true)
+
+	go m.receive(msgch)
+
+	return m
+}
+
+// GetMSPID returns the MSP ID for the given PKI ID
+func (m *mspIDMgr) GetMSPID(pkiID gcommon.PKIidType) string {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+
+	mspID, ok := m.mspIDMap[string(pkiID)]
+	if !ok {
+		logger.Warningf("MSP ID not found for PKI ID [%v]", pkiID)
+	}
+	return mspID
+}
+
+func (m *mspIDMgr) receive(msgch <-chan gossip.ReceivedMessage) {
+	for {
+		logger.Debugf("Listening to gossip messages...\n")
+		select {
+		case msg, ok := <-msgch:
+			if !ok {
+				logger.Infof("Gossip listener terminated")
+				return
+			}
+
+			logger.Debugf("Got gossip message: %v\n", msg)
+			dataUpdate := msg.GetGossipMessage().GetDataUpdate()
+			if dataUpdate != nil {
+				for _, envelope := range dataUpdate.Data {
+					m.handleUpdate(envelope)
+				}
+			}
+		}
+	}
+}
+
+func (m *mspIDMgr) handleUpdate(envelope *gossip.Envelope) {
+	msg, _ := envelope.ToGossipMessage()
+
+	pIdentity := msg.GetPeerIdentity()
+	sID := &msp.SerializedIdentity{}
+	proto.Unmarshal(pIdentity.Cert, sID)
+	pkiID := string(pIdentity.PkiId)
+
+	// Only update if not already in map
+	m.mutex.RLock()
+	mspID, ok := m.mspIDMap[pkiID]
+	m.mutex.RUnlock()
+
+	if !ok || mspID != sID.Mspid {
+		logger.Infof("Mapping PKI ID [%v] to MSP ID [%s]\n", pIdentity.PkiId, sID.Mspid)
+		m.mutex.Lock()
+		m.mspIDMap[pkiID] = sID.Mspid
+		m.mutex.Unlock()
+	}
+}
---
+++ membershipsnap/cmd/mspidmgr_test.go
@@ -0,0 +1,81 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"testing"
+	"time"
+
+	gcommon "github.com/hyperledger/fabric/gossip/common"
+	msppb "github.com/hyperledger/fabric/protos/msp"
+)
+
+// TestMSPIDMgr tests the MSP ID manager
+func TestMSPIDMgr(t *testing.T) {
+	mspID1 := "Org1MSP"
+	mspID2 := "Org2MSP"
+	mspID3 := "Org3MSP"
+
+	pkiID1 := []byte("pki-id-1")
+	pkiID2 := []byte("pki-id-2")
+	pkiID3 := []byte("pki-id-3")
+
+	gossipService := newMockGossipService()
+
+	mspIDMgr := newMSPIDMgr(gossipService)
+
+	time.Sleep(1 * time.Second)
+
+	gossipService.sendMessage(newIdentityMsg(
+		gcommon.PKIidType(pkiID1),
+		&msppb.SerializedIdentity{
+			Mspid:   mspID1,
+			IdBytes: []byte("some-identity"),
+		}))
+	gossipService.sendMessage(newIdentityMsg(
+		gcommon.PKIidType(pkiID2),
+		&msppb.SerializedIdentity{
+			Mspid:   mspID2,
+			IdBytes: []byte("some-identity"),
+		}))
+
+	time.Sleep(1 * time.Second)
+
+	if mspID := mspIDMgr.GetMSPID(pkiID1); mspID != mspID1 {
+		t.Fatalf("Expecting MSP ID [%s] but got [%s]", mspID1, mspID)
+	}
+	if mspID := mspIDMgr.GetMSPID(pkiID2); mspID != mspID2 {
+		t.Fatalf("Expecting MSP ID [%s] but got [%s]", mspID2, mspID)
+	}
+
+	// Send a message that we don't care about - should be ignored
+	gossipService.sendMessage(newAliveMsg())
+
+	time.Sleep(1 * time.Second)
+
+	// Send a new MSP ID for an existing PKI ID and expect it to change
+	gossipService.sendMessage(newIdentityMsg(
+		gcommon.PKIidType(pkiID1),
+		&msppb.SerializedIdentity{
+			Mspid:   mspID3,
+			IdBytes: []byte("some-identity"),
+		}))
+
+	time.Sleep(1 * time.Second)
+
+	if mspID := mspIDMgr.GetMSPID(pkiID1); mspID != mspID3 {
+		t.Fatalf("Expecting MSP ID [%s] but got [%s]", mspID3, mspID)
+	}
+
+	// Retrieve an unknown PKI ID
+	if mspID := mspIDMgr.GetMSPID(pkiID3); mspID != "" {
+		t.Fatalf("Expecting MSP ID [] but got [%s]", mspID)
+	}
+
+	gossipService.Stop()
+	time.Sleep(1 * time.Second)
+}
