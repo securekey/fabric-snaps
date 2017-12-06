9299f6b6539c5beb8cb46ac6bb7961571bac1423
Bob Stasyszyn <bob.stasyszyn@securekey.com>
2017-12-06 09:45:51 -0500

    [DEV-3851] - Snap Config Tool
    CLI to update, query, and delete configuration
    in the ledger.
    
    Change-Id: Id7b5b4b1712a092041aa9f458389cdded1454160
    Signed-off-by: Bob Stasyszyn <bob.stasyszyn@securekey.com>

---
+++ configurationsnap/cmd/configcli/configcli.go
@@ -0,0 +1,49 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"os"
+
+	"github.com/securekey/fabric-snaps/configurationsnap/cmd/configcli/cliconfig"
+	"github.com/securekey/fabric-snaps/configurationsnap/cmd/configcli/deletecmd"
+	"github.com/securekey/fabric-snaps/configurationsnap/cmd/configcli/querycmd"
+	"github.com/securekey/fabric-snaps/configurationsnap/cmd/configcli/updatecmd"
+	"github.com/spf13/cobra"
+)
+
+func newConfigCLICmd() *cobra.Command {
+	mainCmd := &cobra.Command{
+		Use: "configcli",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.HelpFunc()(cmd, args)
+		},
+	}
+
+	flags := mainCmd.PersistentFlags()
+
+	cliconfig.InitLoggingLevel(flags)
+	cliconfig.InitClientConfigFile(flags)
+	cliconfig.InitChannelID(flags)
+	cliconfig.InitUserName(flags)
+	cliconfig.InitUserPassword(flags)
+	cliconfig.InitOrgID(flags)
+	cliconfig.InitMspID(flags)
+
+	mainCmd.AddCommand(querycmd.Cmd(), updatecmd.Cmd(), deletecmd.Cmd())
+
+	return mainCmd
+}
+
+func main() {
+	if newConfigCLICmd().Execute() != nil {
+		os.Exit(1)
+	}
+}
---
+++ configurationsnap/cmd/configcli/querycmd/querycmd.go
@@ -0,0 +1,129 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package querycmd
+
+import (
+	"encoding/json"
+
+	"github.com/hyperledger/fabric-sdk-go/pkg/errors"
+	"github.com/securekey/fabric-snaps/configurationsnap/cmd/configcli/action"
+	"github.com/securekey/fabric-snaps/configurationsnap/cmd/configcli/cliconfig"
+	"github.com/spf13/cobra"
+)
+
+const description = `
+The query command allows the client to query the org's configuration using a Config Key.
+The Config Key consists of:
+
+* MspID (mandatory) - The MSP ID of the organization
+* PeerID (optional) - The ID of the peer
+* AppName (optional) - The application name
+
+The Config Key may be specified as a JSON string (using the --configkey option) or it may
+be specified using the options: --mspid, --peerid, and --appname.
+
+If PeerID and AppName are not specified then all of the org's configuration is returned.
+`
+
+const examples = `
+- Query a single peer for configuration of a particular application:
+    $ ./configcli query --clientconfig ../../../bddtests/fixtures/clientconfig/config.yaml --cid mychannel --peerurl grpcs://localhost:7051 --mspid Org1MSP --peerid peer0.org1.example.com --appname myapp
+
+... results in the following output:
+
+    --------------------------------------------------------------------
+    ----- MSPID: Org1MSP, Peer: peer0.org1.example.com, App: myapp:
+    embedded config
+    --------------------------------------------------------------------
+
+- To display the output in raw format:
+    $ ./configcli query --clientconfig ../../../bddtests/fixtures/clientconfig/config.yaml --cid mychannel --peerurl grpcs://localhost:7051 --mspid Org1MSP --peerid peer0.org1.example.com --appname myapp --format raw
+
+... results in the following output (note that this string would need to be unmarshalled using json.Unmarshal in order to get a readable config Value):
+
+    [{"Key":{"MspID":"Org1MSP","PeerID":"peer0.org1.example.com","AppName":"myapp"},"Value":"ZW1iZWRkZWQgY29uZmln"}]
+
+- Query a single peer for all configuration for Org1MSP:
+    $ ./configcli query --clientconfig ../../../bddtests/fixtures/clientconfig/config.yaml --cid mychannel --peerurl grpcs://localhost:7051 --mspid Org1MSP
+
+- Query a single peer using a config key:
+    $ ./configcli query --clientconfig ../../../bddtests/fixtures/clientconfig/config.yaml --cid mychannel --peerurl grpcs://localhost:7051 --configkey '{"MspID":"Org1MSP","PeerID":"peer0.org1.example.com","AppName":"app1"}'
+`
+
+// Cmd returns the Query command
+func Cmd() *cobra.Command {
+	return newCmd(action.New())
+}
+
+type queryAction struct {
+	action.Action
+}
+
+func newCmd(baseAction action.Action) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "query",
+		Short:   "Query configuration",
+		Long:    description,
+		Example: examples,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			action, err := newQueryAction(baseAction)
+			if err != nil {
+				return errors.Wrap(err, "error while initializing queryAction")
+			}
+			if len(action.Peers()) == 0 {
+				return errors.New("please specify an orgid, mspid, or a peer to connect to")
+			}
+			return action.query()
+		},
+	}
+
+	flags := cmd.Flags()
+
+	cliconfig.InitPeerURL(flags)
+	cliconfig.InitChannelID(flags)
+	cliconfig.InitConfigKey(flags)
+	cliconfig.InitPeerID(flags)
+	cliconfig.InitAppName(flags)
+	cliconfig.InitOutputFormat(flags)
+
+	return cmd
+}
+
+func newQueryAction(baseAction action.Action) (*queryAction, error) {
+	action := &queryAction{
+		Action: baseAction,
+	}
+	err := action.Initialize()
+	return action, err
+}
+
+func (a *queryAction) query() error {
+	key, err := a.ConfigKey()
+	if err != nil {
+		return err
+	}
+
+	if key.MspID == "" {
+		return errors.New("invalid config key: MspID not specified")
+	}
+
+	configKeyBytes, err := json.Marshal(key)
+	if err != nil {
+		return errors.Wrapf(err, "error marshalling config key")
+	}
+
+	cliconfig.Config().Logger().Debugf("Using config key: %s\n", configKeyBytes)
+
+	response, err := a.Query(cliconfig.ConfigSnapID, "get", [][]byte{[]byte(configKeyBytes)})
+	if err != nil {
+		return err
+	}
+
+	Print(response)
+
+	return nil
+}
