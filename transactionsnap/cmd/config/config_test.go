e31dcff640b6b66e86f9c69bf9f8b03c26403bcf
Baha Shaaban <baha.shaaban@securekey.com>
2018-01-24 16:52:36 -0500

    [SNAPS-47] update Fabric Snaps with latest Go SDK changes
    Change-Id: Icd4119250d96574f423cb43b2b3ee31b3f579942
    Signed-off-by: Baha Shaaban <baha.shaaban@securekey.com>

--- transactionsnap/cmd/config/config_test.go
+++ transactionsnap/cmd/config/config_test.go
@@ -7,7 +7,9 @@ SPDX-License-Identifier: Apache-2.0
 package config
 
 import (
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -27,6 +29,20 @@ var coreConfig *viper.Viper
 var c transactionsnapApi.Config
 var channelID = "testChannel"
 var mspID = "Org1MSP"
+var rawCert = "-----BEGIN CERTIFICATE-----\n" +
+	"MIICNjCCAd2gAwIBAgIRAMnf9/dmV9RvCCVw9pZQUfUwCgYIKoZIzj0EAwIwgYEx\n" +
+	"CzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQHEw1TYW4g\n" +
+	"RnJhbmNpc2NvMRkwFwYDVQQKExBvcmcxLmV4YW1wbGUuY29tMQwwCgYDVQQLEwND\n" +
+	"T1AxHDAaBgNVBAMTE2NhLm9yZzEuZXhhbXBsZS5jb20wHhcNMTcxMTEyMTM0MTEx\n" +
+	"WhcNMjcxMTEwMTM0MTExWjBpMQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZv\n" +
+	"cm5pYTEWMBQGA1UEBxMNU2FuIEZyYW5jaXNjbzEMMAoGA1UECxMDQ09QMR8wHQYD\n" +
+	"VQQDExZwZWVyMC5vcmcxLmV4YW1wbGUuY29tMFkwEwYHKoZIzj0CAQYIKoZIzj0D\n" +
+	"AQcDQgAEZ8S4V71OBJpyMIVZdwYdFXAckItrpvSrCf0HQg40WW9XSoOOO76I+Umf\n" +
+	"EkmTlIJXP7/AyRRSRU38oI8Ivtu4M6NNMEswDgYDVR0PAQH/BAQDAgeAMAwGA1Ud\n" +
+	"EwEB/wQCMAAwKwYDVR0jBCQwIoAginORIhnPEFZUhXm6eWBkm7K7Zc8R4/z7LW4H\n" +
+	"ossDlCswCgYIKoZIzj0EAwIDRwAwRAIgVikIUZzgfuFsGLQHWJUVJCU7pDaETkaz\n" +
+	"PzFgsCiLxUACICgzJYlW7nvZxP7b6tbeu3t8mrhMXQs956mD4+BoKuNI\n" +
+	"-----END CERTIFICATE-----"
 
 func TestGetMspID(t *testing.T) {
 	value := c.GetMspID()
@@ -35,19 +51,90 @@ func TestGetMspID(t *testing.T) {
 	}
 }
 
-func TestGetTLSRootCertPath(t *testing.T) {
+func TestGetTLSRootCert(t *testing.T) {
 	value := c.GetTLSRootCertPath()
+	// Test GetTLSRootCertPath()
 	if value != c.GetConfigPath(coreConfig.GetString("peer.tls.rootcert.file")) {
 		t.Fatalf("Expected GetTLSRootCertPath() return value %v but got %v",
 			c.GetConfigPath(coreConfig.GetString("peer.tls.rootcert.file")), value)
 	}
+
+	// Test GetTLSRootCert()
+	// first prepare for testing GetTLSRootCert() below by creating a dummycert file
+	value = "./dummyCert.crt"
+	// override config's tls root cert path to test getting a certificate object from real file.
+	coreConfig.Set("peer.tls.rootcert.file", value)
+	// temporarily create a dummy root cert for testing the cert locally
+	// mock a valid cert data for testing..
+	err := ioutil.WriteFile(value, []byte(rawCert), os.ModePerm)
+	defer os.Remove(value)
+
+	if err != nil {
+		t.Fatalf("Failed to create mock root cert file data for testing: %s", err)
+	}
+
+	_, err = ioutil.ReadFile(value)
+
+	if err != nil {
+		t.Fatalf("Failed to read created mock root cert file for testing: %s", err)
+	}
+
+	// now call GetTLSRootCert()
+	cert := c.GetTLSRootCert()
+	if cert == nil {
+		t.Fatalf("Expected to get non nil tls root cert")
+	}
+
+	// get the real certificate object from rawCert and compare it with 'cert' variable
+	block, _ := pem.Decode([]byte(rawCert))
+	pub, err := x509.ParseCertificate(block.Bytes)
+
+	if !cert.Equal(pub) {
+		t.Fatalf("Expected to get '%s' for the tls root cert, but got: '%s'", pub.Raw, cert.Raw)
+	}
 }
 
-func TestGetTLSCertPath(t *testing.T) {
+func TestGetTLSCert(t *testing.T) {
 	value := c.GetTLSCertPath()
+	// Test GetTLSCertPath()
 	if value != c.GetConfigPath(coreConfig.GetString("peer.tls.cert.file")) {
 		t.Fatalf("Expected GetTLSCertPath() return value %v but got %v",
 			c.GetConfigPath(coreConfig.GetString("peer.tls.cert.file")), value)
+	}
+
+	// Test GetTLSCert()
+	// first prepare for testing GetTLSCert() below by creating a dummycert file
+	value = "./dummayCert.crt"
+	// override config's tls cert path to test getting a certificate object from real file.
+	coreConfig.Set("peer.tls.cert.file", value)
+	// temporarily create a dummy root cert for testing the cert locally
+	// mock a valid cert data for testing..
+	err := ioutil.WriteFile(value, []byte(rawCert), os.ModePerm)
+	defer os.Remove(value)
+
+	if err != nil {
+		t.Fatalf("Failed to create mock root cert file data for testing: %s", err)
+	}
+
+	_, err = ioutil.ReadFile(value)
+
+	if err != nil {
+		t.Fatalf("Failed to read created mock root cert file for testing: %s", err)
+	}
+
+	// now call GetTLSCert()
+	cert := c.GetTLSCert()
+
+	if cert == nil {
+		t.Fatalf("Expected to get non nil tls cert")
+	}
+
+	// get the real certificate object from rawCert and compare it with 'cert' variable
+	block, _ := pem.Decode([]byte(rawCert))
+	pub, err := x509.ParseCertificate(block.Bytes)
+
+	if !cert.Equal(pub) {
+		t.Fatalf("Expected to get '%s' for the tls root cert, but got: '%s'", pub.Raw, cert.Raw)
 	}
 }
 
