b479d3d39d585dc5f2143ea3e1d4de627f82aa49
biljana lukovic <biljana.lukovic@securekey.com>
2018-01-30 16:41:29 -0500

    [DEV-5211]Configure BCCSP provider for BDD test
    Change-Id: Idbe7395c5f7f4216645aaa80a25f4af20ba98261
    Signed-off-by: biljana lukovic <biljana.lukovic@securekey.com>

--- bddtests/context.go
+++ bddtests/context.go
@@ -8,10 +8,17 @@ package bddtests
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	sdkApi "github.com/hyperledger/fabric-sdk-go/api/apifabclient"
 	"github.com/hyperledger/fabric-sdk-go/pkg/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+	"github.com/hyperledger/fabric/bccsp/factory"
+	"github.com/hyperledger/fabric/bccsp/pkcs11"
+	"github.com/pkg/errors"
+	"github.com/securekey/fabric-snaps/transactionsnap/cmd/client/factories"
+	"github.com/spf13/viper"
 )
 
 var orgname = "peerorg1"
@@ -36,14 +43,17 @@ func NewBDDContext() (*BDDContext, error) {
 }
 
 func (b *BDDContext) beforeScenario(scenarioOrScenarioOutline interface{}) {
+	//to initialize BCCSP factory based on config options
+	if err := initializeFactory(); err != nil {
+		panic(fmt.Sprintf("Failed to initialize BCCSP factory %v", err))
+	}
 
 	confileFilePath := "./fixtures/clientconfig/config.yaml"
 
-	sdk, err := fabsdk.New(config.FromFile(confileFilePath))
+	sdk, err := fabsdk.New(config.FromFile(confileFilePath), fabsdk.WithCorePkg(&factories.DefaultCryptoSuiteProviderFactory{}))
 	if err != nil {
 		panic(fmt.Sprintf("Failed to create new SDK: %s", err))
 	}
-
 	// Create SDK setup for the integration tests
 	b.Sdk = sdk
 
@@ -56,7 +66,6 @@ func (b *BDDContext) beforeScenario(scenarioOrScenarioOutline interface{}) {
 	if err != nil {
 		panic(fmt.Sprintf("Failed to create new client for userSession of orgAdmin1: %s", err))
 	}
-
 	b.Org1Admin = client.IdentityContext()
 	b.clients[b.Org1Admin] = client
 
@@ -93,4 +102,78 @@ func (b *BDDContext) beforeScenario(scenarioOrScenarioOutline interface{}) {
 func (b *BDDContext) afterScenario(interface{}, error) {
 	// Holder for common functionality
 
+}
+
+func initializeFactory() error {
+	//read BCCSP config from client config file and intiailize BCCSP factory
+	//this test does not support the PLUGIN option
+	cViper := viper.New()
+	cViper.SetConfigType("yaml")
+	cViper.AddConfigPath("./fixtures/clientconfig")
+	viper.SetConfigName("config")
+	viper.SetEnvPrefix("core")
+	cViper.AutomaticEnv()
+
+	if err := cViper.ReadInConfig(); err != nil {
+		panic(fmt.Sprintf("Failed to read client config file: %v", err))
+	}
+	configuredProvider := cViper.GetString("client.BCCSP.Security.Provider")
+	var opts *factory.FactoryOpts
+	lib := FindPKCS11Lib(cViper.GetString("client.BCCSP.Security.Library"))
+	ksPath := cViper.GetString("client.BCCSP.Security.KeystorePath")
+	level := cViper.GetInt("client.BCCSP.Security.Level")
+	alg := cViper.GetString("client.BCCSP.Security.HashAlgorithm")
+	pin := cViper.GetString("client.BCCSP.Security.Pin")
+	label := cViper.GetString("client.BCCSP.Security.Label")
+	logger.Debugf("Configured BCCSP provider \nlib %s \npin %s \nlabel %s", lib, pin, label)
+
+	switch configuredProvider {
+	case "PKCS11":
+		opts = &factory.FactoryOpts{
+			ProviderName: "PKCS11",
+			Pkcs11Opts: &pkcs11.PKCS11Opts{
+				SecLevel:   level,
+				HashFamily: alg,
+				Ephemeral:  false,
+				Library:    lib,
+				Pin:        pin,
+				Label:      label,
+				FileKeystore: &pkcs11.FileKeystoreOpts{
+					KeyStorePath: ksPath,
+				},
+			},
+		}
+	case "SW":
+		opts = &factory.FactoryOpts{
+			ProviderName: "SW",
+			SwOpts: &factory.SwOpts{
+				HashFamily: alg,
+				SecLevel:   level,
+				Ephemeral:  true,
+			},
+		}
+	default:
+		return errors.New("Unsupported PKCS11 provider")
+	}
+	factory.InitFactories(opts)
+	return nil
+
+}
+
+//FindPKCS11Lib find lib based on configuration
+func FindPKCS11Lib(configuredLib string) string {
+	logger.Debugf("PKCS library configurations paths  %s ", configuredLib)
+	var lib string
+	if configuredLib != "" {
+		possibilities := strings.Split(configuredLib, ",")
+		for _, path := range possibilities {
+			trimpath := strings.TrimSpace(path)
+			if _, err := os.Stat(trimpath); !os.IsNotExist(err) {
+				lib = trimpath
+				break
+			}
+		}
+	}
+	logger.Debugf("Found pkcs library '%s'", lib)
+	return lib
 }
