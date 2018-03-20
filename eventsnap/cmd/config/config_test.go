a097292328e8589743e999a0e1be453b6b0fef40
Sudesh Shetty <sudesh.shetty@securekey.com>
2018-03-20 14:51:42 -0400

    [SNAPS-51] Fixing unit-tests in eventsnap
    Change-Id: I0938edc0b90140654daed514c6f40da017eaf0ef
    Signed-off-by: Sudesh Shetty <sudesh.shetty@securekey.com>

--- eventsnap/cmd/config/config_test.go
+++ eventsnap/cmd/config/config_test.go
@@ -6,111 +6,98 @@ SPDX-License-Identifier: Apache-2.0
 
 package config
 
-//import (
-//	"fmt"
-//	"io/ioutil"
-//	"testing"
-//	"time"
-//
-//	configmocks "github.com/securekey/fabric-snaps/configmanager/pkg/mocks"
-//	"github.com/securekey/fabric-snaps/configmanager/pkg/service"
-//	"github.com/spf13/viper"
-//)
-//
-//func TestInvalidConfig(t *testing.T) {
-//	_, err := New("", "./invalid")
-//	if err == nil {
-//		t.Fatalf("Expecting error for invalid config but received none")
-//	}
-//}
-//
-//func TestConfig(t *testing.T) {
-//	mspID := "Org1MSP"
-//	peerID := "peer1"
-//	channelID1 := "ch1"
-//	channelID2 := "ch2"
-//	channelID3 := "ch3"
-//
-//	configStub1 := configmocks.NewMockStub(channelID1)
-//	service.Initialize(configStub1, mspID)
-//
-//	// Test with no channel config
-//	config, err := New("", "../sampleconfig")
-//	if err == nil {
-//		t.Fatalf("Expecting error creating new config with no channel")
-//	}
-//	// if config.ChannelConfigLoaded {
-//	// 	t.Fatalf("Expecting that channel config is not loaded")
-//	// }
-//	// checkString(t, "EventHubAddress", config.EventHubAddress, "0.0.0.0:7053")
-//
-//	// Test config on channel1
-//	if err := configmocks.SaveConfigFromFile(configStub1, mspID, peerID, EventSnapAppName, "../sampleconfig/configch1.yaml"); err != nil {
-//		t.Fatalf("Error saving config: %s", err)
-//	}
-//	config, err = New(channelID1, "../sampleconfig")
-//	if err != nil {
-//		t.Fatalf("Error creating new config: %s", err)
-//	}
-//	checkString(t, "EventHubAddress", config.EventHubAddress, "0.0.0.0:7053")
-//	checkUint(t, "EventConsumerBufferSize", config.EventConsumerBufferSize, 100)
-//	checkDuration(t, "EventHubRegTimeout", config.EventHubRegTimeout, 1*time.Second)
-//	checkUint(t, "EventDispatcherBufferSize", config.EventDispatcherBufferSize, 100)
-//	checkDuration(t, "EventConsumerTimeout", config.EventConsumerTimeout, 10*time.Millisecond)
-//
-//	// Test config on channel2
-//	configStub2 := configmocks.NewMockStub(channelID2)
-//	if err := configmocks.SaveConfigFromFile(configStub2, mspID, peerID, EventSnapAppName, "../sampleconfig/configch2.yaml"); err != nil {
-//		t.Fatalf("Error saving config: %s", err)
-//	}
-//	config, err = New(channelID2, "../sampleconfig")
-//	if err != nil {
-//		t.Fatalf("Error creating new config: %s", err)
-//	}
-//	checkString(t, "EventHubAddress", config.EventHubAddress, "0.0.0.0:7053")
-//	checkUint(t, "EventConsumerBufferSize", config.EventConsumerBufferSize, 200)
-//	checkDuration(t, "EventHubRegTimeout", config.EventHubRegTimeout, 2*time.Second)
-//	checkUint(t, "EventDispatcherBufferSize", config.EventDispatcherBufferSize, 200)
-//	checkDuration(t, "EventConsumerTimeout", config.EventConsumerTimeout, 20*time.Millisecond)
-//
-//	// Test config on channel3
-//	configStub3 := configmocks.NewMockStub(channelID3)
-//	if err := configmocks.SaveConfigFromFile(configStub3, mspID, peerID, EventSnapAppName, "../sampleconfig/configch3.yaml"); err != nil {
-//		t.Fatalf("Error saving config: %s", err)
-//	}
-//	config, err = New(channelID3, "../sampleconfig")
-//	if err != nil {
-//		t.Fatalf("Error creating new config: %s", err)
-//	}
-//	// try to load the config in a new viper instance and verify the cert pem is loaded
-//	// as we don't have access to the cert/key in config.TransportCredentials
-//	v := viper.New()
-//	v.SetConfigFile("../sampleconfig/configch3.yaml")
-//	v.ReadInConfig()
-//	p := v.Get("eventsnap.eventhub.tlsCerts.client.certpem")
-//
-//	if p == "" {
-//		t.Fatalf("certpem is empty when loading from viper")
-//	}
-//
-//	cp, err := ioutil.ReadFile("tls/client_sdk_go.pem")
-//	checkString(t, "eventhub.tlsCerts.client.certpem", p.(string), fmt.Sprintf("%s", cp))
-//}
-//
-//func checkString(t *testing.T, field string, value string, expectedValue string) {
-//	if value != expectedValue {
-//		t.Fatalf("Expecting [%s] for [%s] but got [%s]", expectedValue, field, value)
-//	}
-//}
-//
-//func checkUint(t *testing.T, field string, value, expectedValue uint) {
-//	if value != expectedValue {
-//		t.Fatalf("Expecting [%d] for [%s] but got [%d]", expectedValue, field, value)
-//	}
-//}
-//
-//func checkDuration(t *testing.T, field string, value, expectedValue time.Duration) {
-//	if value != expectedValue {
-//		t.Fatalf("Expecting %d for %s but got %d", expectedValue, field, value)
-//	}
-//}
+import (
+	"testing"
+	"time"
+
+	"os"
+
+	"github.com/hyperledger/fabric/bccsp/factory"
+	configmocks "github.com/securekey/fabric-snaps/configmanager/pkg/mocks"
+	"github.com/securekey/fabric-snaps/configmanager/pkg/service"
+	"github.com/securekey/fabric-snaps/transactionsnap/pkg/txsnapservice"
+)
+
+const (
+	TxnSnapAppName = "txnsnap"
+	channelID      = "testChannel"
+	mspID          = "Org1MSP"
+	peerID         = "peer1"
+)
+
+func TestInvalidConfig(t *testing.T) {
+	_, err := New("", "./invalid")
+	if err == nil {
+		t.Fatalf("Expecting error for invalid config but received none")
+	}
+}
+
+func TestConfig(t *testing.T) {
+
+	configStub1 := configmocks.NewMockStub(channelID)
+	service.Initialize(configStub1, mspID)
+
+	// Test with no channel config
+	config, err := New("", "../sampleconfig")
+	if err == nil {
+		t.Fatalf("Expecting error creating new config with no channel")
+	}
+
+	// Test config on channel1
+	if err := configmocks.SaveConfigFromFile(configStub1, mspID, peerID, EventSnapAppName, "../sampleconfig/config.yaml"); err != nil {
+		t.Fatalf("Error saving config: %s", err)
+	}
+	if err := configmocks.SaveConfigFromFile(configStub1, mspID, peerID, TxnSnapAppName, "../sampleconfig/txnsnap-config.yaml"); err != nil {
+		t.Fatalf("Error saving config: %s", err)
+	}
+	config, err = New(channelID, "../sampleconfig")
+	if err != nil {
+		t.Fatalf("Error creating new config: %s", err)
+	}
+	checkUint(t, "EventConsumerBufferSize", config.EventConsumerBufferSize, 101)
+	checkUint(t, "EventDispatcherBufferSize", config.EventDispatcherBufferSize, 101)
+	checkDuration(t, "EventConsumerTimeout", config.EventConsumerTimeout, 11*time.Millisecond)
+	checkDuration(t, "EventConsumerTimeout", config.ResponseTimeout, 3*time.Second)
+	checkString(t, "EventDispatcherBufferSize", config.URL, "0.0.0.0:7051")
+
+	if len(config.Bytes) == 0 {
+		t.Fatal("config bytes are not supposed to be empty")
+	}
+
+}
+
+func checkString(t *testing.T, field string, value string, expectedValue string) {
+	if value != expectedValue {
+		t.Fatalf("Expecting [%s] for [%s] but got [%s]", expectedValue, field, value)
+	}
+}
+
+func checkUint(t *testing.T, field string, value, expectedValue uint) {
+	if value != expectedValue {
+		t.Fatalf("Expecting [%d] for [%s] but got [%d]", expectedValue, field, value)
+	}
+}
+
+func checkDuration(t *testing.T, field string, value, expectedValue time.Duration) {
+	if value != expectedValue {
+		t.Fatalf("Expecting %d for %s but got %d", expectedValue, field, value)
+	}
+}
+
+func TestMain(m *testing.M) {
+
+	txsnapservice.PeerConfigPath = "../sampleconfig"
+
+	opts := &factory.FactoryOpts{
+		ProviderName: "SW",
+		SwOpts: &factory.SwOpts{
+			HashFamily:   "SHA2",
+			SecLevel:     256,
+			Ephemeral:    false,
+			FileKeystore: &factory.FileKeystoreOpts{KeyStorePath: "../sampleconfig/msp/keystore"},
+		},
+	}
+	factory.InitFactories(opts)
+
+	os.Exit(m.Run())
+}
--- eventsnap/cmd/eventsnap_test.go
+++ eventsnap/cmd/eventsnap_test.go
@@ -6,180 +6,160 @@ SPDX-License-Identifier: Apache-2.0
 
 package main
 
-//import (
-//	"fmt"
-//	"testing"
-//	"time"
-//
-//	"github.com/securekey/fabric-snaps/configmanager/pkg/service"
-//	localservice "github.com/securekey/fabric-snaps/eventservice/pkg/localservice"
-//	"github.com/securekey/fabric-snaps/eventsnap/cmd/config"
-//
-//	"github.com/hyperledger/fabric/core/chaincode/shim"
-//	configmocks "github.com/securekey/fabric-snaps/configmanager/pkg/mocks"
-//)
-//
-//func TestEventSnap(t *testing.T) {
-//	// ehMx := &sync.RWMutex{}
-//	channelID1 := "ch1"
-//	channelID2 := "ch2"
-//
-//	// config0, err := config.New("", "./sampleconfig", newMockConfigServiceProvider()())
-//	// if err != nil {
-//	// 	t.Fatalf("Error getting config for channel [%s]: %s", "", err)
-//	// }
-//
-//	peerID := "peer1"
-//	mspID := "Org1MSP"
-//	configStub1 := configmocks.NewMockStub(channelID1)
-//	configStub1.ChannelID = channelID1
-//	service.Initialize(configStub1, mspID)
-//
-//	if err := configmocks.SaveConfigFromFile(configStub1, mspID, peerID, config.EventSnapAppName, "./sampleconfig/configch1.yaml"); err != nil {
-//		t.Fatalf("Error saving config: %s", err)
-//	}
-//
-//	eventsnap := &eventSnap{
-//		peerConfigPath: "./sampleconfig",
-//	}
-//
-//	stub := shim.NewMockStub("eventsnap", eventsnap)
-//	// Invalid options
-//	stub.ChannelID = channelID1
-//	if resp := stub.MockInit("txid2", nil); resp.Status == shim.OK {
-//		t.Fatalf("Expecting error in init since no event hub address was specified but got OK")
-//	}
-//
-//	// // mockEventHubs := make(map[string]*mockeventhub.MockEventHub)
-//	// // configProvider.setConfig("", config0)
-//	// eventsnap = &eventSnap{
-//	// 	// eropts: eventrelay.MockOpts(func(channelID string, address string, regTimeout time.Duration, adapter eventrelay.EventAdapter, tlsConfig *tls.Config) (eventrelay.EventHub, error) {
-//	// 	// 	fmt.Printf("Creating mock event hub for channel %s\n", channelID)
-//	// 	// 	mockeh := mockeventhub.New(adapter)
-//	// 	// 	ehMx.Lock()
-//	// 	// 	mockEventHubs[channelID] = mockeh
-//	// 	// 	ehMx.Unlock()
-//	// 	// 	return mockeh, nil
-//	// 	// }),
-//	// 	configProvider: configProvider,
-//	// }
-//
-//	// stub = shim.NewMockStub("eventsnap", eventsnap)
-//
-//	// Initialize
-//	stub.ChannelID = channelID1
-//	if resp := stub.MockInit("txid3", nil); resp.Status != shim.OK {
-//		t.Fatalf("Error in init: %s", resp.GetMessage())
-//	}
-//
-//	// Another channel
-//	stub.ChannelID = channelID2
-//	// configProvider.setConfig("ch2", config2)
-//	if resp := stub.MockInit("txid4", nil); resp.Status != shim.OK {
-//		t.Fatalf("Error in init: %s", resp.GetMessage())
-//	}
-//
-//	// Delay adding the configuration
-//	time.Sleep(8 * time.Second)
-//
-//	// configProvider.setConfig(channelID1, config1)
-//	// configProvider.setConfig(channelID2, config1)
-//
-//	// Wait for the event snap to pick up the configuration
-//	time.Sleep(6 * time.Second)
-//
-//	// Init again on same channel
-//	if resp := stub.MockInit("txid5", nil); resp.Status == shim.OK {
-//		t.Fatalf("Expecting error in init since init was already called for the same channel but got OK")
-//	}
-//
-//	// Invoke should return error
-//	if resp := stub.MockInvoke("txid6", nil); resp.Status == shim.OK {
-//		t.Fatalf("Expecting error in invoke since invoke is not supported but got OK")
-//	}
-//
-//	eventService1 := localservice.Get(channelID1)
-//	if eventService1 == nil {
-//		t.Fatalf("Expecting local event service for %s to be registered but got nil", channelID1)
-//	}
-//	eventService2 := localservice.Get(channelID2)
-//	if eventService2 == nil {
-//		t.Fatalf("Expecting local event service for %s to be registered but got nil", channelID2)
-//	}
-//	reg1, bEventCh1, err := eventService1.RegisterBlockEvent()
-//	if err != nil {
-//		t.Fatalf("Error in RegisterBlockEvent on event service channel %s: %s", channelID1, err)
-//	}
-//	defer eventService1.Unregister(reg1)
-//
-//	reg2, bEventCh2, err := eventService2.RegisterBlockEvent()
-//	if err != nil {
-//		t.Fatalf("Error in RegisterBlockEvent on event service channel %s: %s", channelID2, err)
-//	}
-//	defer eventService2.Unregister(reg2)
-//
-//	// ehMx.RLock()
-//	// mockEventHubs[channelID1].ProduceEvent(mockevent.NewBlockEvent(channelID1))
-//	// mockEventHubs[channelID2].ProduceEvent(mockevent.NewBlockEvent(channelID2))
-//	// ehMx.RUnlock()
-//
-//	numExpected := 2
-//	numReceived := 0
-//	done := false
-//
-//	for !done {
-//		select {
-//		case event, ok := <-bEventCh1:
-//			if !ok {
-//				t.Fatalf("event channel1 disconnected")
-//			}
-//			fmt.Printf("*** Received event on bEventCh1: %v\n", event)
-//			numReceived++
-//		case event, ok := <-bEventCh2:
-//			if !ok {
-//				t.Fatalf("event channel2 disconnected")
-//			}
-//			fmt.Printf("*** Received event on bEventCh2: %s\n", event)
-//			numReceived++
-//		case <-time.After(2 * time.Second):
-//			if numReceived != numExpected {
-//				t.Fatalf("Expecting %d events but received %d", numExpected, numReceived)
-//			} else {
-//				done = true
-//			}
-//		}
-//	}
-//}
-//
-//// func newMockConfig(channelID string, configPath string, option string) (*config.EventSnapConfig, error) {
-//
-//// 	esconfig := &config.EventSnapConfig{
-//// 		MSPID:           "Org1MSP",
-//// 		EventHubAddress: "localhost:9053",
-//// 	}
-//
-//// 	fileName := configPath + "/config" + channelID + ".yaml"
-//// 	sdkConfigBytes, err := ioutil.ReadFile(fileName)
-//// 	if err != nil {
-//// 		panic(fmt.Sprintf("Got error reading config file [%s]: %s", fileName, err))
-//// 	}
-//// 	esconfig.Bytes = sdkConfigBytes
-//
-//// 	switch option {
-//// 	case "emptyAddress":
-//// 		esconfig.EventHubAddress = ""
-//// 		esconfig.ChannelConfigLoaded = true
-//
-//// 	default:
-//// 		if esconfig != nil && channelID != "" {
-//// 			esconfig.ChannelConfigLoaded = true
-//// 			esconfig.EventHubRegTimeout = time.Duration(1 * time.Second)
-//// 			esconfig.EventHubRegTimeout = time.Duration(1 * time.Second)
-//// 			esconfig.EventDispatcherBufferSize = uint(100)
-//// 			esconfig.EventConsumerBufferSize = uint(100)
-//// 			esconfig.EventConsumerTimeout = time.Duration(10 * time.Millisecond)
-//// 		}
-//// 	}
-//
-//// 	return esconfig, nil
-//// }
+import (
+	"os"
+	"testing"
+
+	"github.com/hyperledger/fabric/bccsp/factory"
+	"github.com/securekey/fabric-snaps/configmanager/pkg/service"
+	"github.com/securekey/fabric-snaps/eventservice/pkg/localservice"
+	"github.com/securekey/fabric-snaps/eventsnap/cmd/config"
+
+	"strings"
+
+	fcmocks "github.com/hyperledger/fabric-sdk-go/pkg/fab/mocks"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	configmocks "github.com/securekey/fabric-snaps/configmanager/pkg/mocks"
+	"github.com/securekey/fabric-snaps/transactionsnap/pkg/txsnapservice"
+)
+
+const (
+	TxnSnapAppName       = "txnsnap"
+	channelID1           = "testchannel"
+	channelID2           = "testChannel2"
+	mspID                = "Org1MSP"
+	peerID               = "peer1"
+	eventSvcExistsErrMsg = "Event service already initialized for channel"
+)
+
+func TestEventSnap(t *testing.T) {
+
+	configStub1 := configmocks.NewMockStub(channelID1)
+	configStub1.ChannelID = channelID1
+	service.Initialize(configStub1, mspID)
+
+	configStub2 := configmocks.NewMockStub(channelID2)
+	configStub2.ChannelID = channelID2
+	service.Initialize(configStub2, mspID)
+
+	if err := configmocks.SaveConfigFromFile(configStub1, mspID, peerID, config.EventSnapAppName, "./sampleconfig/config.yaml"); err != nil {
+		t.Fatalf("Error saving config: %s", err)
+	}
+	if err := configmocks.SaveConfigFromFile(configStub2, mspID, peerID, config.EventSnapAppName, "./sampleconfig/config.yaml"); err != nil {
+		t.Fatalf("Error saving config: %s", err)
+	}
+	if err := configmocks.SaveConfigFromFile(configStub1, mspID, peerID, TxnSnapAppName, "./sampleconfig/txnsnap-config.yaml"); err != nil {
+		t.Fatalf("Error saving config: %s", err)
+	}
+	if err := configmocks.SaveConfigFromFile(configStub2, mspID, peerID, TxnSnapAppName, "./sampleconfig/txnsnap-config.yaml"); err != nil {
+		t.Fatalf("Error saving config: %s", err)
+	}
+
+	eventsnap := &eventSnap{
+		peerConfigPath: "./sampleconfig",
+	}
+
+	stub := shim.NewMockStub("eventsnap", eventsnap)
+
+	// Start mock event hub
+	eventServer, err := fcmocks.StartMockEventServer("127.0.0.1:7051")
+	if err != nil {
+		t.Fatalf("Failed to start mock event hub: %v", err)
+	}
+	defer eventServer.Stop()
+
+	// Happy Path
+	stub.ChannelID = channelID1
+	if resp := stub.MockInit("txid2", nil); resp.Status != shim.OK {
+		t.Fatalf("Error in init: %s", resp.GetMessage())
+	}
+
+	// Initialize again with same channel
+	if resp := stub.MockInit("txid3", nil); resp.Status == shim.OK || !strings.Contains(resp.GetMessage(), eventSvcExistsErrMsg) {
+		t.Fatalf("Expected '%s', but got '%s'", eventSvcExistsErrMsg, resp.GetMessage())
+	}
+
+	// Another channel
+	stub.ChannelID = channelID2
+	if resp := stub.MockInit("txid4", nil); resp.Status != shim.OK {
+		t.Fatalf("Error in init: %s", resp.GetMessage())
+	}
+
+	// Init again on same channel
+	if resp := stub.MockInit("txid5", nil); resp.Status == shim.OK || !strings.Contains(resp.GetMessage(), eventSvcExistsErrMsg) {
+		t.Fatalf("Expected '%s', but got '%s'", eventSvcExistsErrMsg, resp.GetMessage())
+	}
+
+	eventService1 := localservice.Get(channelID1)
+	if eventService1 == nil {
+		t.Fatalf("Expecting local event service for %s to be registered but got nil", channelID1)
+	}
+	eventService2 := localservice.Get(channelID2)
+	if eventService2 == nil {
+		t.Fatalf("Expecting local event service for %s to be registered but got nil", channelID2)
+	}
+
+	//TODO :  event service currently doesn't support block event (commenting below tests)
+
+	//reg1, bEventCh1, err := eventService1.RegisterBlockEvent()
+	//if err != nil {
+	//	t.Fatalf("Error in RegisterBlockEvent on event service channel %s: %s", channelID1, err)
+	//}
+	//defer eventService1.Unregister(reg1)
+	//
+	//reg2, bEventCh2, err := eventService2.RegisterBlockEvent()
+	//if err != nil {
+	//	t.Fatalf("Error in RegisterBlockEvent on event service channel %s: %s", channelID2, err)
+	//}
+	//defer eventService2.Unregister(reg2)
+	//
+	//// ehMx.RLock()
+	//// mockEventHubs[channelID1].ProduceEvent(mockevent.NewBlockEvent(channelID1))
+	//// mockEventHubs[channelID2].ProduceEvent(mockevent.NewBlockEvent(channelID2))
+	//// ehMx.RUnlock()
+	//
+	//numExpected := 2
+	//numReceived := 0
+	//done := false
+	//
+	//for !done {
+	//	select {
+	//	case event, ok := <-bEventCh1:
+	//		if !ok {
+	//			t.Fatalf("event channel1 disconnected")
+	//		}
+	//		fmt.Printf("*** Received event on bEventCh1: %v\n", event)
+	//		numReceived++
+	//	case event, ok := <-bEventCh2:
+	//		if !ok {
+	//			t.Fatalf("event channel2 disconnected")
+	//		}
+	//		fmt.Printf("*** Received event on bEventCh2: %s\n", event)
+	//		numReceived++
+	//	case <-time.After(2 * time.Second):
+	//		if numReceived != numExpected {
+	//			t.Fatalf("Expecting %d events but received %d", numExpected, numReceived)
+	//		} else {
+	//			done = true
+	//		}
+	//	}
+	//}
+}
+
+func TestMain(m *testing.M) {
+
+	txsnapservice.PeerConfigPath = "./sampleconfig"
+
+	opts := &factory.FactoryOpts{
+		ProviderName: "SW",
+		SwOpts: &factory.SwOpts{
+			HashFamily:   "SHA2",
+			SecLevel:     256,
+			Ephemeral:    false,
+			FileKeystore: &factory.FileKeystoreOpts{KeyStorePath: "./sampleconfig/msp/keystore"},
+		},
+	}
+	factory.InitFactories(opts)
+
+	os.Exit(m.Run())
+}
