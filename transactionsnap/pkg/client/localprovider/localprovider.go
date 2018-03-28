b50df9a5acb1e74b253c5609a4e766220ddf6c46
Firas Qutishat <firas.qutishat@securekey.com>
2018-03-28 16:14:51 -0400

    Add tls cert for local peer discovery
    Change-Id: I994c70e4539d6ad11ec0c22dcb11695f56700c08
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- transactionsnap/pkg/client/localprovider/localprovider.go
+++ transactionsnap/pkg/client/localprovider/localprovider.go
@@ -12,6 +12,7 @@ import (
 	logging "github.com/hyperledger/fabric-sdk-go/pkg/common/logging"
 	coreApi "github.com/hyperledger/fabric-sdk-go/pkg/common/providers/core"
 	fabApi "github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
+	"github.com/hyperledger/fabric-sdk-go/pkg/core/config/endpoint"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fab/peer"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk/factory/defsvc"
 	"github.com/securekey/fabric-snaps/transactionsnap/api"
@@ -22,19 +23,21 @@ var logger = logging.NewLogger("txnsnap")
 // Factory is configured with local provider
 type Factory struct {
 	defsvc.ProviderFactory
-	LocalPeer *api.PeerConfig
+	LocalPeer           *api.PeerConfig
+	LocalPeerTLSCertPem []byte
 }
 
 // CreateDiscoveryProvider returns a new implementation of dynamic discovery provider
 func (l *Factory) CreateDiscoveryProvider(config coreApi.Config, fabPvdr fabApi.InfraProvider) (fabApi.DiscoveryProvider, error) {
 	logger.Debug("create local Provider Impl")
-	return &impl{config, l.LocalPeer}, nil
+	return &impl{config, l.LocalPeer, l.LocalPeerTLSCertPem}, nil
 }
 
 // impl implements a LocalProviderFactory
 type impl struct {
-	clientConfig coreApi.Config
-	localPeer    *api.PeerConfig
+	clientConfig        coreApi.Config
+	localPeer           *api.PeerConfig
+	localPeerTLSCertPem []byte
 }
 
 // CreateDiscoveryService return impl of local discovery service
@@ -55,6 +58,8 @@ func (s *localDiscoveryService) GetPeers() ([]fabApi.Peer, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error get peer config by url: %v", err)
 	}
+	peerConfig.TLSCACerts = endpoint.TLSConfig{Pem: string(s.localPeerTLSCertPem)}
+
 	peer, err := peer.New(s.clientConfig, peer.FromPeerConfig(&coreApi.NetworkPeer{PeerConfig: *peerConfig, MSPID: string(s.localPeer.MSPid)}))
 	if err != nil {
 		return nil, fmt.Errorf("error creating new peer: %v", err)
