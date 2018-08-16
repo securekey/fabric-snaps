5fb6798a97af4988e7f752438c97f06a8c8ba325
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2018-08-16 11:47:30 -0400

    [DEV-8930] Use cache for blockchain info
    Change-Id: I49b4ba533f8e27af9e1a16f898263a91eee2712f
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

--- util/bcinfo/bcinfo.go
+++ util/bcinfo/bcinfo.go
@@ -7,21 +7,63 @@ SPDX-License-Identifier: Apache-2.0
 package bcinfo
 
 import (
+	"time"
+
+	"github.com/hyperledger/fabric-sdk-go/pkg/util/concurrent/lazycache"
+	"github.com/hyperledger/fabric-sdk-go/pkg/util/concurrent/lazyref"
 	"github.com/hyperledger/fabric/core/peer"
 	cb "github.com/hyperledger/fabric/protos/common"
 )
 
-// Provider is a Blockchain Info Provider
+const (
+	defaultRefreshInterval = time.Second
+)
+
+// Provider implements a caching Blockchain Info Provider
 type Provider struct {
-	bcInfo map[string]*cb.BlockchainInfo
+	cache *lazycache.Cache
 }
 
-// NewProvider returns a new Blockchain Info Provider
+// ledger is an abstraction of the peer ledger
+type ledger interface {
+	GetBlockchainInfo() (*cb.BlockchainInfo, error)
+}
+
+// ledgerProvider returns a ledger
+type ledgerProvider func(channelID string) ledger
+
+// Singleton provider
+var provider = createProvider()
+
+// ledgerPrvdr returns the peer ledger
+var ledgerPrvdr = func(channelID string) ledger {
+	return peer.GetLedger(channelID)
+}
+
+// NewProvider returns a Blockchain Info Provider
 func NewProvider() *Provider {
-	return &Provider{}
+	return provider
 }
 
 // GetBlockchainInfo delegates to the peer to return basic info about the blockchain
 func (l *Provider) GetBlockchainInfo(channelID string) (*cb.BlockchainInfo, error) {
-	return peer.GetLedger(channelID).GetBlockchainInfo()
+	bcinfo, err := l.cache.Get(lazycache.NewStringKey(channelID))
+	if err != nil {
+		return nil, err
+	}
+	return bcinfo.(*cb.BlockchainInfo), nil
+}
+
+func createProvider() *Provider {
+	// TODO: Make refresh interval configurable
+	refreshInterval := defaultRefreshInterval
+
+	return &Provider{
+		cache: lazycache.New("bcinfo_cache",
+			func(key lazycache.Key) (interface{}, error) {
+				return ledgerPrvdr(key.String()).GetBlockchainInfo()
+			},
+			lazyref.WithRefreshInterval(lazyref.InitImmediately, refreshInterval),
+		),
+	}
 }
