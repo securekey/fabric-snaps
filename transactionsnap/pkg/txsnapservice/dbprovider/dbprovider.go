6ff54314075e0b92041de909dfa4f425d2078ff0
Firas Qutishat <firas.qutishat@securekey.com>
2018-12-10 15:04:38 -0500

    Get db provider from statecachedstore
    Change-Id: Icb0ff9183afc1ae094a8e33fafe6e104217d57c7
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- transactionsnap/pkg/txsnapservice/dbprovider/dbprovider.go
+++ transactionsnap/pkg/txsnapservice/dbprovider/dbprovider.go
@@ -10,21 +10,24 @@ import (
 	"sync"
 
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/logging"
-	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/privacyenabledstate"
+	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/statedb"
+	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/statedb/statecachedstore"
+	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/statedb/statecouchdb"
+	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/statedb/statekeyindex"
 	"github.com/hyperledger/fabric/core/ledger/ledgerconfig"
 	"github.com/securekey/fabric-snaps/util/errors"
 )
 
 var logger = logging.NewLogger("txnsnap")
 
-var stateDBProvider privacyenabledstate.DBProvider
+var stateDBProvider statedb.VersionedDBProvider
 var dbProviderErr error
 var once sync.Once
 
 // GetStateDB returns a handle to the local state database. Only CouchDB is supported.
 // connections to the database are cached.
 // The handle may be closed and discarded after use.
-func GetStateDB(channelID string) (privacyenabledstate.DB, error) {
+func GetStateDB(channelID string) (statedb.VersionedDB, error) {
 	dbProvider, err := getStateDBProviderInstance()
 	if err != nil {
 		return nil, err
@@ -40,17 +43,23 @@ func GetStateDB(channelID string) (privacyenabledstate.DB, error) {
 	return db, nil
 }
 
-func getStateDBProviderInstance() (privacyenabledstate.DBProvider, error) {
+func getStateDBProviderInstance() (statedb.VersionedDBProvider, error) {
 	if !ledgerconfig.IsCouchDBEnabled() {
 		return nil, errors.Errorf(errors.SystemError, "Local query is only supported on CouchDB")
 	}
 
 	once.Do(func() {
 		logger.Info("Creating StateDB provider")
-		stateDBProvider, dbProviderErr = privacyenabledstate.NewCommonStorageDBProvider()
-		if dbProviderErr != nil {
-			logger.Warnf("Error creating StateDB provider %s", dbProviderErr)
+		vdbProvider, err := statecouchdb.NewVersionedDBProvider()
+		if err != nil {
+			logger.Warnf("Error creating StateDB provider %s", err)
 		}
+		stateKeyIndexProvider := statekeyindex.NewProvider()
+
+		stateDBProvider = statecachedstore.NewProvider(
+			vdbProvider,
+			stateKeyIndexProvider)
+
 	})
 
 	return stateDBProvider, dbProviderErr
