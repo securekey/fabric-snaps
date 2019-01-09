328380ab5d584dd8add2bc3d3124945d9cb9a567
Firas Qutishat <firas.qutishat@securekey.com>
2019-01-09 11:02:49 -0500

    Add FilterMetricName
    Change-Id: Ib57fdb61500d719d292686b4c2ec0875a81b4d41
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- metrics/pkg/util/util.go
+++ metrics/pkg/util/util.go
@@ -7,10 +7,14 @@ SPDX-License-Identifier: Apache-2.0
 package util
 
 import (
+	"regexp"
+
 	"github.com/hyperledger/fabric/common/metrics"
 	"github.com/hyperledger/fabric/common/metrics/disabled"
 	"github.com/hyperledger/fabric/core/peer"
 )
+
+var reg = regexp.MustCompile("[^a-zA-Z0-9_]+")
 
 //GetMetricsInstance return metrics instance
 func GetMetricsInstance() metrics.Provider {
@@ -19,4 +23,8 @@ func GetMetricsInstance() metrics.Provider {
 		metricsInstance = &disabled.Provider{}
 	}
 	return metricsInstance
+}
+
+func FilterMetricName(name string) string {
+	return reg.ReplaceAllString(name, "_")
 }
