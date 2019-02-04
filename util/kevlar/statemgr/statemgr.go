9425d66dc0a83f6d36cdda3eec9fe427bcb81540
Sudesh Shetty <sudesh.shetty@securekey.com>
2019-02-04 16:28:41 -0500

    [DEV-11452] cluster changes - cleanup
    Change-Id: If825f416c1915ff5445c83cfce1037392e258866
    Signed-off-by: Sudesh Shetty <sudesh.shetty@securekey.com>

--- util/kevlar/statemgr/statemgr.go
+++ util/kevlar/statemgr/statemgr.go
@@ -44,19 +44,4 @@ func GetState(channelID, ccNamespace, key string) ([]byte, errors.Error) {
 	logger.Debugf("Query returned %+v for namespace %s and key %s", vv.Value, ccNamespace, key)
 
 	return vv.Value, nil
-
-	//TODO for kevlar
-
-	//ledger, err := ledgermgmt.OpenLedger(channelID)
-	//if err != nil {
-	//	return nil, errors.WithMessage(errors.SystemError, err, "Failed to open ledger")
-	//}
-	//value, err := ledger.GetState(ccNamespace, key)
-	//if err != nil {
-	//	return nil, errors.WithMessage(errors.SystemError, err, "Failed to get state")
-	//}
-	//
-	//logger.Debugf("Query returned %+v for namespace %s and key %s", value, ccNamespace, key)
-	//
-	//return value, nil
 }
