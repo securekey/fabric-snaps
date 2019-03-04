9f20f265967a899f97fd474c47fec9ec8cb08172
Firas Qutishat <firas.qutishat@securekey.com>
2019-03-04 15:34:53 -0500

    Customize fabric-snaps
    Change-Id: I3f4ef9cbe1e0f9ba50fc382299fd1bb7bf57aaab
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- bddtests/fixtures/channel/import-ec-keys.sh
+++ bddtests/fixtures/channel/import-ec-keys.sh
@@ -11,8 +11,6 @@ set -e
 #This is temporary script which will import peer msp key to HSM so that peer can start successfully
 #TODO This script should be removed once required keys are already available in peer HSM
 
-PKC11_TOOL=github.com/gbolo/go-util/p11tool
-GO_SRC=/opt/gopath/src
 
 declare -a PRIVATE_KEYS=()
 
@@ -21,17 +19,12 @@ do
     PRIVATE_KEYS+=($file)
 done
 
-echo "Installing pkcs11 tool..."
-go get ${PKC11_TOOL}
 
 echo "Importing keys to softhsm..."
-softhsm2-util --init-token --slot 1 --label "ForFabric" --pin 98765432 --so-pin 987654
-
-cd ${GO_SRC}/${PKC11_TOOL}
 for i in "${PRIVATE_KEYS[@]}"
 do
     echo "Importing key : ${GO_SRC}${i}"
     openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in ${i} -out private.p8
-    go run main.go -action import -keyFile private.p8
+    pkcs11helper -action import -keyFile private.p8
     rm -rf private.p8
 done
