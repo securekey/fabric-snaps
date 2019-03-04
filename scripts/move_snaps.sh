023fa771e3ff76da40d28636119fc5da6e1b4d3d
Firas Qutishat <firas.qutishat@securekey.com>
2019-03-04 15:34:53 -0500

    Customize fabric-snaps
    Change-Id: I3f4ef9cbe1e0f9ba50fc382299fd1bb7bf57aaab
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/move_snaps.sh
+++ scripts/move_snaps.sh
@@ -8,13 +8,13 @@ set -e
 
 
 echo "Creating temp directories within fabric..."
-mkdir -p /opt/gopath/src/github.com/hyperledger/fabric/plugins/
+mkdir -p $GOPATH/src/github.com/hyperledger/fabric/plugins/
 
 echo "Copying snaps to subdirectory within fabric..."
-cp -r /opt/gopath/src/github.com/securekey/fabric-snaps/* /opt/gopath/src/github.com/hyperledger/fabric/plugins/
+cp -r $GOPATH/src/github.com/securekey/fabric-snaps/* $GOPATH/src/github.com/hyperledger/fabric/plugins/
 
 echo "Rewriting import paths..."
-find /opt/gopath/src/github.com/hyperledger/fabric/plugins -type f -name "*.go" -print0 | xargs -0 sed -i "s/github.com\/securekey\/fabric-snaps\//github.com\/hyperledger\/fabric\/plugins\//g"
+find $GOPATH/src/github.com/hyperledger/fabric/plugins -type f -name "*.go" -print0 | xargs -0 sed -i "s/github.com\/securekey\/fabric-snaps\//github.com\/hyperledger\/fabric\/plugins\//g"
 
-rm -rf /opt/gopath/src/github.com/securekey/fabric-snaps
-rm -rf /opt/gopath/src/github.com/hyperledger/fabric/go.sum
+rm -rf $GOPATH/src/github.com/securekey/fabric-snaps
+rm -rf $GOPATH/src/github.com/hyperledger/fabric/go.sum
