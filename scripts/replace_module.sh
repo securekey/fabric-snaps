7b4634b19c50cf9dd1de076dfd66703b90a66b6c
Firas Qutishat <firas.qutishat@securekey.com>
2019-03-08 12:51:55 -0500

    Fix replace_module script
    Change-Id: I3e25897facb1d72a76e2c5bc5cb9efd5f6bd07a3
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/replace_module.sh
+++ scripts/replace_module.sh
@@ -5,12 +5,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
+sedGoPath=$(echo $GOPATH | sed 's#/#\\/#g')
+
 # replace modules in fabric-snaps go.mod
-sed 's/\github.com\/securekey\/fabric-next.*/..\//g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/go.mod
+sed 's/\github.com\/securekey\/fabric-next.*/$GOPATH\/src\/github.com\/hyperledger\/fabric/g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/go.mod
 sed 's/\github.com\/securekey\/fabric-snaps/github.com\/hyperledger\/fabric\/plugins/g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/go.mod
+sed 's/$GOPATH/'"$sedGoPath"'/' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/go.mod
+
 # replace modules in rolesmgr go.mod
-sed 's/\github.com\/securekey\/fabric-next.*/..\/..\/..\/..\//g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/util/rolesmgr/go.mod
-sed 's/\github.com\/securekey\/fabric-snaps/github.com\/hyperledger\/fabric\/plugins\/util\/rolesmgr/g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/util/rolesmgr/go.mod
+sed 's/\github.com\/securekey\/fabric-next.*/$GOPATH\/src\/github.com\/hyperledger\/fabric/g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/util/rolesmgr/go.mod
+sed 's/\github.com\/securekey\/fabric-snaps\/util/github.com\/hyperledger\/fabric\/plugin\/util/g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/util/rolesmgr/go.mod
+sed 's/$GOPATH/'"$sedGoPath"'/' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/util/rolesmgr/go.mod
+
 # replace modules in statemgr go.mod
-sed 's/\github.com\/securekey\/fabric-next.*/..\/..\/..\/..\//g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/util/statemgr/go.mod
-sed 's/\github.com\/securekey\/fabric-snaps/github.com\/hyperledger\/fabric\/plugins\/util\/statemgr/g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/util/statemgr/go.mod
+sed 's/\github.com\/securekey\/fabric-next.*/$GOPATH\/src\/github.com\/hyperledger\/fabric/g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/util/statemgr/go.mod
+sed 's/\github.com\/securekey\/fabric-snaps\/util/github.com\/hyperledger\/fabric\/plugins\/util/g' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/util/statemgr/go.mod
+sed 's/$GOPATH/'"$sedGoPath"'/' -i $GOPATH/src/github.com/hyperledger/fabric/plugins/util/statemgr/go.mod
