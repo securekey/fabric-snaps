4303e21395f4eb76b22200a950f537e165284e08
Parasuraman Ramalingam <Parasuraman.Ramalingam@securekey.com>
2019-03-20 16:17:37 -0400

    [DEV-12804] Replaced metalinters with golangci-lint command. Fixed all the lint errors.
    Change-Id: Idabc571c21a78c04e5b1b7f0e9ab17d33513b8b5
    Signed-off-by: Parasuraman Ramalingam <Parasuraman.Ramalingam@securekey.com>

--- scripts/check_lint.sh
+++ scripts/check_lint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #
 # Copyright SecureKey Technologies Inc. All Rights Reserved.
 #
@@ -7,19 +8,44 @@
 # This script runs Go linting and vetting tools
 
 set -e
+GO_CMD="${GO_CMD:-go}"
+GOPATH="${GOPATH:-${HOME}/go}"
+GOLANGCI_LINT_CMD=golangci-lint
 
-
-GOMETALINT_CMD=gometalinter
-
-
-function finish {
-  rm -rf vendor
+function installGoPkg {
+    declare repo=$1
+    declare revision=$2
+    declare pkgPath=$3
+    shift 3
+    declare -a cmds=$@
+    echo "Installing ${repo}@${revision} to $GOPATH/bin ..."
+    GO111MODULE=off GOPATH=${BUILD_TMP} go get -d ${repo}
+    tag=$(cd ${BUILD_TMP}/src/${repo} && git tag -l | sort -V --reverse | head -n 1 | grep "${revision}" || true)
+    if [ ! -z "${tag}" ]; then
+        revision=${tag}
+        echo "  using tag ${revision}"
+    fi
+    (cd ${BUILD_TMP}/src/${repo} && git reset --hard ${revision})
+    echo " Checking $GOPATH ..."
+    GO111MODULE=off GOPATH=${BUILD_TMP} go install -i ${repo}/${pkgPath}
+    mkdir -p ${GOPATH}/bin
+    for cmd in ${cmds[@]}
+    do
+        echo "Copying ${cmd} to ${GOPATH}/bin"
+        cp -f ${BUILD_TMP}/bin/${cmd} ${GOPATH}/bin/
+    done
 }
-trap finish EXIT
 
+function installGolangCi {
+    echo "Installing ${GOLANGCI_LINT_CMD} ..."
+    BUILD_TMP=`mktemp -d 2>/dev/null || mktemp -d -t 'fabricsnaps'`
 
-echo "Running metalinters..."
-# metalinters don't work with go modules yet
-# for now we create vendor folder and remove it after running metalinters
-go mod vendor
-GO111MODULE=off $GOMETALINT_CMD --config=./gometalinter.json ./...
+    declare repo="github.com/golangci/golangci-lint/cmd/golangci-lint"
+    declare revision="v1.15.0"
+    installGoPkg "${repo}" "${revision}" "" "${GOLANGCI_LINT_CMD}"
+
+    rm -Rf ${BUILD_TMP}
+}
+
+installGolangCi
+GO111MODULE=on ${GOLANGCI_LINT_CMD} run ./... -c .golangci.yml
