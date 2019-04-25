06d78cf44bb6ffc4ac843da0df413ac477971fcc
Tomisin Jenrola <Tomisin.Jenrola@securekey.com>
2019-04-25 12:42:49 -0400

    [DEV-13296] set fabric config path if it exists
    Change-Id: Iaa9283d37634422272583f69d71c606896d8aa7a
    Signed-off-by: Tomisin Jenrola <tomisin.jenrola@securekey.com>
    (cherry picked from commit 86a8ff8748596235239ab2986c26d1d1e6f4f04a)

--- util/configcache/configcache_test.go
+++ util/configcache/configcache_test.go
@@ -6,7 +6,10 @@ SPDX-License-Identifier: Apache-2.0
 
 package configcache
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestConfigCache(t *testing.T) {
 	cache := New("core", "coreprefix", "./testdata")
@@ -31,4 +34,25 @@ func TestConfigCache(t *testing.T) {
 	if _, err := cache.Get("./invalidpath"); err == nil {
 		t.Fatalf("expecting error getting config for invalid path but got none")
 	}
+}
+
+func TestFabricConfigPath(t *testing.T) {
+	// set fabric config path
+	os.Setenv("FABRIC_CFG_PATH", "./testfabricconfig")
+
+	cache := New("core", "coreprefix", "./testdata")
+
+	config, err := cache.Get("")
+	if err != nil {
+		t.Fatalf("error getting config: %s", err)
+	}
+	if config == nil {
+		t.Fatalf("config is nil")
+	}
+
+	peerid := config.Get("peer.id")
+	if peerid != "ckent" {
+		t.Fatalf("incorrect config loaded")
+	}
+
 }
