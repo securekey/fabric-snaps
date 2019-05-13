983b12b36a43ae918df742177575cd2431856adf
Parasuraman Ramalingam <Parasuraman.Ramalingam@securekey.com>
2019-05-13 11:42:46 -0400

    [DEV-13698] Adding a CommitOnly function
    Change-Id: I0555529ad7ceb3787b5243aebd4f590b1e3cd8cc
    Signed-off-by: Parasuraman Ramalingam <Parasuraman.Ramalingam@securekey.com>

--- bddtests/common_steps.go
+++ bddtests/common_steps.go
@@ -154,7 +154,7 @@ func (d *CommonSteps) createChannelAndJoinPeers(channelID string, orgs []string)
 		if err := d.joinPeersToChannel(channelID, orgID, peersConfig); err != nil {
 			return fmt.Errorf("error joining peer to channel: %s", err)
 		}
-
+		time.Sleep(5 * time.Second)
 	}
 
 	return nil
@@ -304,6 +304,15 @@ func (d *CommonSteps) queryCConOrg(ccID, args, orgIDs, channelID string) error {
 }
 
 func (d *CommonSteps) querySystemCC(ccID, args, orgID, channelID string) error {
+	argsArray := strings.Split(args, ",")
+
+	endorsements := ""
+	tm := make(map[string][]byte, 0)
+	if len(argsArray) > 1 && argsArray[1] == "commitOnlyTransaction" {
+		tm["endorsements"] = []byte(queryValue)
+		endorsements = queryValue
+	}
+
 	queryValue = ""
 
 	peersConfig, ok := d.BDDContext.clientConfig.PeersConfig(orgID)
@@ -315,14 +324,15 @@ func (d *CommonSteps) querySystemCC(ccID, args, orgID, channelID string) error {
 	if str, ok := peersConfig[0].GRPCOptions["ssl-target-name-override"].(string); ok {
 		serverHostOverride = str
 	}
-	argsArray := strings.Split(args, ",")
 
 	var err error
-	queryValue, err = d.QueryCCWithArgs(true, ccID, channelID, argsArray, nil,
+	queryValue, err = d.QueryCCWithArgs(true, ccID, channelID, argsArray, tm,
 		[]*PeerConfig{&PeerConfig{Config: peersConfig[0], OrgID: orgID, MspID: d.BDDContext.peersMspID[serverHostOverride], PeerID: serverHostOverride}}...)
 	if err != nil {
+		fmt.Printf("%s : querySystemCC: args=%s; endorsements=%s; error=%s\n", time.Now().Format(time.RFC3339), args, endorsements, err)
 		return fmt.Errorf("QueryCCWithArgs return error: %s", err)
 	}
+	fmt.Printf("%s : querySystemCC: args=%s; result=%s", time.Now().Format(time.RFC3339), args, queryValue)
 	logger.Debugf("QueryCCWithArgs return value: [%s]", queryValue)
 	return nil
 }
@@ -339,6 +349,9 @@ func (d *CommonSteps) QueryCCWithOpts(systemCC bool, ccID, channelID string, arg
 		return "", errors.New("no targets specified")
 	}
 
+	if len(args) > 0 {
+		fmt.Printf("%s : QueryCCWithOpts: args=%s\n", time.Now().Format(time.RFC3339), strings.Join(args, ","))
+	}
 	var peers []fabApi.Peer
 	var orgID string
 	var queryResult string
@@ -724,6 +737,7 @@ func (d *CommonSteps) RegisterSteps(s *godog.Suite) {
 	s.Step(`^we wait (\d+) seconds$`, d.wait)
 	s.Step(`^client queries chaincode "([^"]*)" with args "([^"]*)" on all peers in the "([^"]*)" org on the "([^"]*)" channel$`, d.queryCConOrg)
 	s.Step(`^client queries system chaincode "([^"]*)" with args "([^"]*)" on org "([^"]*)" peer on the "([^"]*)" channel$`, d.querySystemCC)
+	s.Step(`client queries system chaincode "([^"]*)" with endorsement response and with args "([^"]*)" on org "([^"]*)" peer on the "([^"]*)" channel`, d.querySystemCC)
 	s.Step(`^response from "([^"]*)" to client contains value "([^"]*)"$`, d.containsInQueryValue)
 	s.Step(`^response from "([^"]*)" to client equal value "([^"]*)"$`, d.equalQueryValue)
 	s.Step(`^"([^"]*)" chaincode "([^"]*)" is installed from path "([^"]*)" to all peers$`, d.installChaincodeToAllPeers)
