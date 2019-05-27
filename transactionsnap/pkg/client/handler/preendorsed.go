ed8f05f2ddb072cd764b8e93f24c7051590e553b
Firas Qutishat <firas.qutishat@securekey.com>
2019-05-27 15:38:13 -0400

    Clean up commitOnlyTransaction method
    Change-Id: I06abc8e7443c8bb434c737744623f3f6e6e2ae75
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- transactionsnap/pkg/client/handler/preendorsed.go
+++ transactionsnap/pkg/client/handler/preendorsed.go
@@ -1,22 +1,35 @@
 package handler
 
 import (
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel/invoke"
 )
 
 //NewPreEndorsedHandler returns a handler that populates the Endorsement Response to the request context
-func NewPreEndorsedHandler(response *invoke.Response, next ...invoke.Handler) *PreEndorsedHandler {
-	return &PreEndorsedHandler{response: response, next: getNext(next)}
+func NewPreEndorsedHandler(endorserResponse *channel.Response, next ...invoke.Handler) *PreEndorsedHandler {
+	return &PreEndorsedHandler{endorserResponse: endorserResponse, next: getNext(next)}
 }
 
 //PreEndorsedHandler holds the Endorsement response
 type PreEndorsedHandler struct {
-	next     invoke.Handler
-	response *invoke.Response
+	next             invoke.Handler
+	endorserResponse *channel.Response
 }
 
 //Handle for endorsing transactions
 func (i *PreEndorsedHandler) Handle(requestContext *invoke.RequestContext, clientContext *invoke.ClientContext) {
-	requestContext.Response = *i.response
+
+	requestContext.Response = getResponse(i.endorserResponse)
 	i.next.Handle(requestContext, clientContext)
+}
+
+func getResponse(res *channel.Response) invoke.Response {
+	return invoke.Response{
+		Payload:          res.Payload,
+		ChaincodeStatus:  res.ChaincodeStatus,
+		TransactionID:    res.TransactionID,
+		Responses:        res.Responses,
+		TxValidationCode: res.TxValidationCode,
+		Proposal:         res.Proposal,
+	}
 }
